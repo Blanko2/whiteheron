f83c07857f83b085080667403154f94d6665bca0
silvalex <ss.alexandre.a@gmail.com>
2012-08-16 19:22:23 +1200

    Added code to ColorQuantization module.

--- codeFiles/RiverDog/src/modules/ColorQuantization.java
+++ codeFiles/RiverDog/src/modules/ColorQuantization.java
@@ -3,11 +3,27 @@ package modules;
 import java.awt.image.BufferedImage;
 
 public class ColorQuantization implements Module {
+    public static final int MASK = 0xff808080; // mask used for quantization, keeps the highest bit of R, G and B
+    private BufferedImage original;
 
-	@Override
+	/**
+	 * Applies color quantization to the original image,
+	 * utilizing the constant mask defined in this class.
+	 */
+    @Override
 	public BufferedImage getImage() {
-		// TODO Auto-generated method stub
-		return null;
+	    int w = original.getWidth();
+        int h = original.getHeight();
+        // Create result image
+        BufferedImage result = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        
+        // Go through every pixel of the image
+        for(int x=0; x< w; x++){
+            for(int y=0; y< h; y++){
+                // Apply mask to original value and save it in result image
+                result.setRGB(x,y, original.getRGB(x, y) & MASK);
+            }
+        }
+        return result;
 	}
-
 }
